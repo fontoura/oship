f47cb530664a8920ecbbd36900bf8c8e3022e2a1
tim.cook <>
2008-06-20 06:50:25 +0000

    Package normalization updates

--- oship/src/oship/openehr/rm/common/archetyped/interfaces/archetyped.py
+++ oship/src/oship/openehr/rm/common/archetyped/interfaces/archetyped.py
@@ -18,7 +18,7 @@ Common Information Model Rev. 2.1.0
 __author__  = u'Timothy Cook <timothywayne.cook@gmail.com>'
 __docformat__ = u'plaintext'
 
-from zope.interface import implements,Interface
+from zope.interface import Interface
 from zope.schema import TextLine
 from zope.i18nmessageid import MessageFactory
 
@@ -68,29 +68,3 @@ class IArchetyped(Interface):
     def rmVersionValid():
         """ rmVersion != None and rmVersion != '' """
 
-        
-class Archetyped(Locatable):
-    """
-    Archetypes act as the configuration basis for the particular structures of instances
-    defined by the reference model. To enable archetypes to be used to create valid
-    data, key classes in the reference model act as “root” points for archetyping;
-    accordingly, these classes have the archetype_details attribute set. An instance of
-    the class ARCHETYPED contains the relevant archetype identification information,
-    allowing generating archetypes to be matched up with data instances
-    """
-    
-    implements(IArchetyped)
-            
-    def __init__(self,atid,tmplid,rmver,**kw):
-        self.archetypeId=atid
-        self.templateId=tmplid
-        self.rmVersion=rmver
-        for n,v in kw.items():
-            setattr(self,n,v)
-        
-
-    def archetypeIdValid():
-        """ archetypeId != None """
-        
-    def rmVersionValid():
-        """ rmVersion != None and rmVersion != '' """

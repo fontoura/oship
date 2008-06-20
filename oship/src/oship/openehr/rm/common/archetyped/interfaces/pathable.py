a8ab081c5745825a7896bc1bf22af2f236034fcd
tim.cook <>
2008-06-20 06:59:06 +0000

    Package normalization updates

--- oship/src/oship/openehr/rm/common/archetyped/interfaces/pathable.py
+++ oship/src/oship/openehr/rm/common/archetyped/interfaces/pathable.py
@@ -18,10 +18,8 @@ Common Information Model Rev. 2.1.0
 __author__  = u'Timothy Cook <timothywayne.cook@gmail.com>'
 __docformat__ = u'plaintext'
 
-from zope.interface import implements
-from zope.location import Location,ILocation
+from zope.location import ILocation
 from zope.i18nmessageid import MessageFactory
-
 
 _ = MessageFactory('oship')
 
@@ -80,57 +78,3 @@ class IPathable(ILocation):
         True if the path corresponds to a single item in the data.
         Return a_path != None and pathExists(a_path)
         """
-
-class Pathable(Location):
-    """
-    Abstract parent of all classes whose instances are reachable by paths, and which
-    know how to locate child object by paths. The parent feature may be implemented 
-    as a function or attribute.
-    
-    The two attributes required for locatable in ZCA is __parent__ and __name__.  
-    We inherit those from Location.
-    
-    The functionality to get paths and find children is contained in the traversal mechanism.
-    """
-    
-    implements(IPathable)
-        
-    def pathOfItem(an_item):
-        """
-        The path to an item relative to the root of this archetyped structure.
-        
-        getPath is from the Traversal API.
-        """
-                       
-    def itemAtPath(a_path):
-        """
-        The item at a path (relative to this item);only valid for unique paths, i.e. paths
-        that resolve to a single item.
-        a_path is a string.
-        Return a_path != None and pathUnique(a_path)
-        
-        If the path is not unique or not found then a TraversalError is raised.
-        """       
-        
-        
-    def itemsAtPath(a_path):
-        """
-        List of items corresponding to a non-unique path.
-        a_path is a List
-        Return a_path != None and pathUnique(a_path)
-        """
-        
- 
-    def pathExists(a_path):
-        """
-        True if the path exists in the data with respect to the current item.
-        Return a_path != None and a_path != '' 
-        """
-        
-        
-    def pathUnique(a_path):
-        """
-        True if the path corresponds to a single item in the data.
-        Return a_path != None and pathExists(a_path)
-        """
- 

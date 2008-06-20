761e8ea0f56fd503d589b7849d0971722ac0977f
tim.cook <>
2008-06-20 06:55:51 +0000

    Package normalization updates

--- oship/src/oship/openehr/rm/common/archetyped/interfaces/link.py
+++ oship/src/oship/openehr/rm/common/archetyped/interfaces/link.py
@@ -18,13 +18,13 @@ Common Information Model Rev. 2.1.0
 __author__  = u'Timothy Cook <timothywayne.cook@gmail.com>'
 __docformat__ = u'plaintext'
 
-from zope.interface import implements,Interface
+from zope.interface import Interface
 from zope.schema import TextLine
 from zope.i18nmessageid import MessageFactory
 
-from openehr.rm.common.locatable import Locatable,ILocatable
-from openehr.rm.datatypes.dvtext import DvText
-from openehr.rm.datatypes.dvehruri import DvEhrUri
+from openehr.rm.common.archetyped.interfaces.locatable import ILocatable
+from openehr.rm.datatypes.text.dvtext import DvText
+from openehr.rm.datatypes.uri.dvehruri import DvEhrUri
 
 _ = MessageFactory('oship')
         
@@ -82,39 +82,4 @@ class ILink(ILocatable):
     def targetValid():
         """Return target != None """
 
-        
-        
-class Link(Locatable):
-    """
-    The LINK type defines a logical relationship between two items, such as two
-    ENTRYs or an ENTRY and a COMPOSITION. Links can be used across composi-
-    tions, and across EHRs. Links can potentially be used between interior (i.e. non
-    archetype root) nodes, although this probably should be prevented in archetypes.
-    Multiple LINKs can be attached to the root object of any archetyped structure to
-    give the effect of a 1->N link 1:1 and 1:N relationships between archetyped content 
-    elements (e.g. ENTRYs) can be expressed by using one, or more than one, respectively, DV_LINKs.
-    Chains of links can be used to see “problem threads” or other logical groupings of items.
-    Links should be between archetyped structures only, i.e. between objects representing 
-    complete domain concepts because relationships between sub-elements
-    of whole concepts are not necessarily meaningful, and may be downright confusing. Sensible 
-    links only exist between whole ENTRYs, SECTIONs, COMPOSITIONs and so on.
-    """
     
-    implements(ILink)
-    
-    def __init__(self,meaning,type,target,**kw):
-        self.meaning=meaning
-        self.type=type
-        self.target=target
-        for n,v in kw.items():
-            setattr(self,n,v)
-     
-    def meaningValid():
-        """Return meaning != None """
-        
-    def typeValid():
-        """Return type != None """
-        
-    def targetValid():
-        """Return target != None """
-

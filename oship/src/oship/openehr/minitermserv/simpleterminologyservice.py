0fb5f34db23fd968d70b36caead7cefb9697f08a
tim.cook <>
2008-09-16 14:11:24 +0000

    Corrected syntax and indent errors

--- oship/src/oship/openehr/minitermserv/simpleterminologyservice.py
+++ oship/src/oship/openehr/minitermserv/simpleterminologyservice.py
@@ -35,7 +35,7 @@ class SimpleTerminologyService():
 
     implements(TerminologyService)
 
-        __filename = '/openehr_terminology_en.xml'
+    __filename = '/openehr_terminology_en.xml'
 
     def terminology(name):
         u"""
@@ -45,10 +45,10 @@ class SimpleTerminologyService():
 
         name != None and name is a valid TerminologyAccess.        
         """
-            try:
-                return self.terminology[name]
-            except KeyError:
-                return None
+        try:
+            return self.terminology[name]
+        except KeyError:
+            return None
 
     def codeSet(name):
         u"""
@@ -56,10 +56,10 @@ class SimpleTerminologyService():
 
         name != None and hasCodeSet == True.
         """
-            try:
-                return self.codeSets[name]
-            except KeyError:
-                return None
+        try:
+            return self.codeSets[name]
+        except KeyError:
+            return None
 
     def codeSetForId(id):
         u""" 
@@ -67,11 +67,11 @@ class SimpleTerminologyService():
 
         id != None and validCodeSetId(id) == True
         """
-            try:
-                name = codeSetInternalIdToExternalName[id]
-                return self.codeSets[name]
-            except KeyError:
-                return None
+        try:
+            name = codeSetInternalIdToExternalName[id]
+            return self.codeSets[name]
+        except KeyError:
+            return None
 
     def hasTerminology(name):
         u"""
@@ -96,52 +96,52 @@ class SimpleTerminologyService():
         Set (LIST) of all terminology identifiers known in the terminology service. Values from
         the US NLM UMLS meta-data list at http://www.nlm.nih.gov/research/umls/metaa1.html
         """
-            return [k for k,v in self.terminologies.items()]
+        return [k for k,v in self.terminologies.items()]
 
     def codeSetIdentifiers():
         u"""
         Set of all code set identifiers known in the terminology service.
         """
-            return [k for k,v in self.codeSets.items()]        
+        return [k for k,v in self.codeSets.items()]        
 
     def openehrCodeSets():
         u"""
         Set of all code sets identifiers for which there is an internal openEHR name;
         returned as a Hash of ids keyed by internal name.
         """
-            return self.codeSetInternalIdToExternalName
+        return self.codeSetInternalIdToExternalName
 
-        def getInstance(x = SimpleTerminologyService):
-            try:
-                single = x()
-            except SimpleTerminologyService, s:
-                single = s
-            return single
+    def getInstance(x = SimpleTerminologyService):
+        try:
+            single = x()
+        except SimpleTerminologyService, s:
+            single = s
+        return single
 
-        def __init__(self):
-            terminologySource = XMLTerminologySource(filename)		
-            __loadTerminologies(terminologySource)
-            __loadCodeSets(terminologySource)			
+    def __init__(self):
+        terminologySource = XMLTerminologySource(filename)		
+        __loadTerminologies(terminologySource)
+        __loadCodeSets(terminologySource)			
 
-        def __loadTerminologies(source):
-            terminologies = {}		
-            terminology = SimpleTerminologyAccess('openehr');
-            groups = source.groupList
-            for group in groups:
-                codes = []
-                names = {}
-                names["en"] = group.name
-                for concept in group.concepts:
-                    codes.append(concept.id)
-                    terminology.addRubric("en", concept.id, concept.rubric)
-                terminology.addGroup(group.name, codes, names);	
-            terminologies['openehr'] = terminology
+    def __loadTerminologies(source):
+        terminologies = {}		
+        terminology = SimpleTerminologyAccess('openehr');
+        groups = source.groupList
+        for group in groups:
+            codes = []
+            names = {}
+            names["en"] = group.name
+            for concept in group.concepts:
+                codes.append(concept.id)
+                terminology.addRubric("en", concept.id, concept.rubric)
+            terminology.addGroup(group.name, codes, names);	
+        terminologies['openehr'] = terminology
 
-        def __loadCodeSets(self, source) {
-            self.codeSets = {}
-            self.codeSetInternalIdToExternalName = {}
-            for codeSet in source.codeSetList:
-                simpleCodeSet = SimpleCodeSetAccess(codeset.externalId, codeset.codes)
-                self.codeSets[codeset.externalId] = codeSetAccess)
-                self.codeSetInternalIdToExternalName[codeset.openehrId] = codeset.externalId)
+    def __loadCodeSets(self, source):
+        self.codeSets = {}
+        self.codeSetInternalIdToExternalName = {}
+        for codeSet in source.codeSetList:
+            simpleCodeSet = SimpleCodeSetAccess(codeset.externalId, codeset.codes)
+            self.codeSets[codeset.externalId] = codeSetAccess)
+            self.codeSetInternalIdToExternalName[codeset.openehrId] = codeset.externalId)
 

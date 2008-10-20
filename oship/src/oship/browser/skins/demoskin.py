a844928aba54125155f9ceb76c6d55dd9cdec5b5
tim.cook <>
2008-10-20 23:52:52 +0000

    Cleaningup to prepare for the merge from the grok branch.

--- oship/src/oship/browser/skins/demoskin.py
+++
@@ -1,6 +0,0 @@
-import z3c.formui.interfaces
-
-from oship.browser.layers.demolayer import IDemoarchetypeBrowserLayer
-
-class IDemoarchetypeBrowserSkin(z3c.formui.interfaces.IDivFormLayer,IDemoarchetypeBrowserLayer):
-    """The browser skin using the div-based layout."""

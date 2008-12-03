62499d903e485179df0ecb8098e32965e95a7164
rui.boon <>
2008-12-03 05:59:55 +0000

    Adds the ability to add, edit and display limited fields of archetypes using webforms.
    Currently, only schema.TextLine and schema.Bool can be displayed. As such, to see the forms in action, certain temporary adjustments have to be made. Not wanting to pollute the codebase with this, these adjustments have to be manually applied using webformDemo.diff
    
    The addForm is located at http://localhost:8080/oship/@@add and the list of archetype entries stored is at http://localhost:8080/oship/@@arlist
    
    Cheers
    Rui Boon

--- oship/src/oship/app.py
+++ oship/src/oship/app.py
@@ -2,12 +2,13 @@ import grok
 from datetime import datetime 
 
 from oship.openehr.am.archetype.interfaces.archetype import IArchetype
+from oship.km.openehr.ehr.cluster.checklist_item_general import ChecklistItemGeneral
 
 class Oship(grok.Application, grok.Container):
     pass
 
 class Index(grok.View):
-    
+    grok.context(Oship)
     def current_datetime(self):
         now = datetime.now()
         return now.strftime('%Y-%m-%d %H:%M')
@@ -22,11 +23,13 @@ class ATView(grok.View):
 
 class ARList(grok.View):
     """ Show the Archetype Repository Contents"""
+    grok.context(Oship)
+    pass
 
-    def render(self):
-        return "Here we'll show a list of Archetypes"
+    #def render(self):
+    #    return "Here we'll show a list of Archetypes"
 
-    
+
 class ArchetypeDetails(grok.View):
     """
     Returns a dict of attributes and its value of an object, obj
@@ -64,4 +67,55 @@ class ArchetypeDetails(grok.View):
         Returns the attributes of module.arche_name
         """
         return self.getAttributes(getattr(module,arche_name)())
+
+
+## Currently not in used
+## TODO: Put all archetype models under this container
+## for easier management
+## class ArchetypeRepository(grok.Container):
+##    pass
+
+class Add(grok.AddForm):
+    grok.context(Oship)
+
+    form_fields = grok.Fields(IArchetype)
+
+    @grok.action(u"Add archetype")
+    def add_archetype(self, **data):
+        # create an archetype instance
+        ## TODO: Find a way to determine the type of archetype
+        ## and dynamically import and create its instance
+        archetype = ChecklistItemGeneral()
+
+        # assign the right attributes to fulfill IArchetype schema with
+        # the form data
+        self.applyData(archetype, **data)
+
+        # Stores the instance
+        # TODO: Select a better key name
+        name = data['__name__']
+        # Stores the instance under the var name, name
+        # TODO: check for duplication of key before storing it
+        # to avoid DuplicationError 
+        self.context[name] = archetype
+
+        # redirect to the newly created object
+        self.redirect(self.url(archetype))
+        # we don't want to display anything, as we redirect
+        return ''
+
+class Edit(grok.EditForm):
+    grok.context(IArchetype)
+
+    form_fields = grok.Fields(IArchetype)
+
+    @grok.action(u"Edit archetype")
+    def edit_archetype(self, **data):
+        self.applyData(ChecklistItemGeneral(), **data)
+
+class Display(grok.DisplayForm):
+    grok.context(IArchetype)
+    grok.name('index')
+
+    form_fields = grok.Fields(IArchetype)
 

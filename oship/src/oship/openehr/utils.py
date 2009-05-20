40f1a6670f22902b087c83a7a45ff1e44ad66c6a
Tim Cook <timothywayne.cook@gmail.com>
2009-05-20 07:43:25 -0300

    Updates to OSHIP app_developers.pdf

--- oship/src/oship/openehr/utils.py
+++ oship/src/oship/openehr/utils.py
@@ -17,8 +17,7 @@ A set of utility functions needed mostly for processing archetypes at runtime.
 
 def Languages(): # enter the language codes into this list for your application(s).
     """
-    As of OSHIP 1.0a3 I have removed all languages except international English.
-    I have added registration for English for the i18nmessage Factory.  
+    As of OSHIP 1.0a3 I have added registration for English for the i18nmessage Factory.  
     We need a tool to auto extract any additional languages and create .po files.
     """
     
